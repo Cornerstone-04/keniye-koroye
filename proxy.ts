669c446c4c9b9865a0116a346243f4bb5bfd4196
Cornerstone <thecornerstoneephraim@gmail.com>
2026-08-21 19:52:55 +0100

    update network proxy

--- proxy.ts
+++ proxy.ts
@@ -13,3 +13,9 @@ export function proxy(request: NextRequest) {
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|avif)$).*)",
+  ],
+};
